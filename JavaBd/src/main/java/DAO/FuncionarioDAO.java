0ff2343c28a70804ba529b39a471e98adc3ebe1e
Gasparsk <143566536+Gasparsk@users.noreply.github.com>
2024-11-05 22:23:01 -0300

    Add files via upload

---
+++ JavaBd/src/main/java/DAO/FuncionarioDAO.java
@@ -0,0 +1,48 @@
+package DAO;
+
+import DTO.FuncionarioDTO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class FuncionarioDAO {
+
+    Connection conn;
+    PreparedStatement pstm;
+    
+    public void cadastrarFuncionario(FuncionarioDTO funcionariodto) {
+
+        String sql = "insert into tabela(nome, cidade)values (?,?)";
+        
+        conn = new Conexao().conectaBd();
+        
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, funcionariodto.getNomeFuncionario());
+            pstm.setString(2, funcionariodto.getCidadeFuncionario());
+            pstm.execute();
+            pstm.close();
+        } catch (Exception e) {
+            
+            JOptionPane.showMessageDialog(null, e + "FuncionarioDAO");
+        }
+        
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
